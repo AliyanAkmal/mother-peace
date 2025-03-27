5172d9f729d2092638148275f4900a5f1342b40f
kanjutajmal <kanjutajmal@gmail.com>
2025-03-27 22:27:08 +0500

    achievements pages

--- src/app/achievements/allachievements/page.js
+++ src/app/achievements/allachievements/page.js
@@ -1,7 +1,15 @@
+import cardsData from "@/components/achivementsdata"
+import AchivementsMap from "@/components/achivementsmap"
 
 export default function AllAchievements() {
   return (
-    <div className="p-3 bg-[#F8F8F8]">
-        Your Achievements
+    <div className="p-3 bg-[#F8F8F8] min-h-screen">
+      <h2 className="text-4xl font-semibold">Your Achievements</h2>
+      <h4 className="text-xl mt-2">
+        John Doe
+      </h4>
+      <div>
+        <AchivementsMap data={cardsData}/>
+      </div>
     </div>
-  )}
+  )} 
--- src/app/achievements/monthlychallenges/page.js
+++ src/app/achievements/monthlychallenges/page.js
@@ -1,7 +1,32 @@
-
+import cardsData from "@/components/achivementsdata"
+import { Calendar } from "lucide-react";
+import AchivementsMap from "@/components/achivementsmap"
 export default function MonthlyChallenges() {
-  return (
-    <div>
-        MonthlyChallengess
+return (
+    <div className="p-3 bg-[#F8F8F8] space-y-8 min-h-screen">
+      <div className="space-y-2">
+         <h2 className="text-4xl font-semibold">Your Achievements</h2>
+       <h4 className="text-xl ">
+         John Doe
+      </h4>
+      </div>
+       <div className="bg-white space-y-8 rounded-lg p-4 lg:p-8 text-white">
+        <div className="flex flex-col ">
+            <div className="flex  gap-2">
+             <Calendar size={30} strokeWidth={1} stroke="black " />             
+             <div className="mt-[-8px]">
+              <h4 className="text-[28px] text-[#28303F] font-semibold">May 2025 Challenges</h4>
+              <span className="text-[#28303F] text-base">
+                You're close to earning these achievements
+              </span>
+             </div>
+             </div>
+        </div>
+        <div>
+            <AchivementsMap data={cardsData.slice(0-3)}/>
+        </div>
     </div>
-  )}
+      <p className="text-[#5E8DE5] pl-4 text-base">New challenges are released at the beginning of each month. Complete all challenges to earn the "Monthly Champion" badge.</p>
+
+    </div>
+  )} 
