c9d8ae3ec91b8d740a694d5c27542e8a07fc235a
AliyanAkmal <aliyanakmal2002@gmail.com>
2025-04-17 18:41:30 +0500

    saaf

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function HomePage() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
+
+  return <div className="w-screen h-screen bg-white"></div>;
+}
